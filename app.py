29ca254e01c7682a331c3c95930b98060560d11a
gfourie23 <135474497+gfourie23@users.noreply.github.com>
2023-08-15 14:21:43 -0600

    Add files via upload

--- app.py
+++ app.py
@@ -1,14 +1,51 @@
-from flask import Flask
+# Put your app in here.
+from flask import Flask, request
+from operations import add, sub, mult, div
 app = Flask(__name__)
 
-@app.route('/welcome')
-def welcome():
-    return "Welcome"
+@app.route('/add')
+def add():
+    a = int(request.args.get("a"))
+    b = int(request.args.get("b"))
+    result = add(a, b)
 
-@app.route('/welcome/home')
-def welcome_home():
-    return "Welcome home"
+    return str(result)
 
-@app.route('/welcome/back')
-def welcome_back():
-    return "Welcome back"
+@app.route('/sub')
+def sub():
+    a = int(request.args.get("a"))
+    b = int(request.args.get("b"))
+    result = sub(a, b)
+
+    return str(result)
+
+@app.route('/mult')
+def mult():
+    a = int(request.args.get("a"))
+    b = int(request.args.get("b"))
+    result = mult(a, b)
+
+    return str(result)
+
+@app.route('/div')
+def div():
+    a = int(request.args.get("a"))
+    b = int(request.args.get("b"))
+    result = div(a, b)
+
+    return str(result)
+   
+operators = {
+    "add":add,
+    "sub": sub,
+    "mult": mult,
+    "div": div,
+}
+
+@app.route("/math/<oper>")
+def math(oper):
+    a = int(request.args.get("a"))
+    b = int(request.args.get("b"))
+    result = operators[oper](a,b)
+
+    return str(result)
